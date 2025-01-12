0176b1a72ee7408ece9516f356206bccc17989b0
hy43 <hyde0401@gmail.com>
2025-01-12 22:10:34 +0900

    refactoring : extract amountFor method

--- src/main/java/refactoring/app/domain/Statement.java
+++ src/main/java/refactoring/app/domain/Statement.java
@@ -1,37 +1,14 @@
 package refactoring.app.domain;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.Locale;
-
 public class Statement {
     StringBuilder statement(Invoice invoice, Plays plays) throws Exception {
         double totalAmount = 0;
         int volumeCredits = 0;
         StringBuilder result = new StringBuilder(String.format("청구 내역 (고객명: %s)\n", invoice.getCustomer()));
 
-        NumberFormat numberFormat = new DecimalFormat("0.##");
         for (Performance performance : invoice.getPerformances()) {
             Play play = plays.findByPlayID(performance.getPlayID());
-            int thisAmount = 0;
-
-            switch (play.getType()) {
-                case "tragedy": // 비극
-                    thisAmount = 40000;
-                    if (performance.getAudience() > 30) {
-                        thisAmount += 1000 * (performance.getAudience() - 30);
-                    }
-                    break;
-                case "comedy": // 희극
-                    thisAmount = 30000;
-                    if (performance.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (performance.getAudience() - 20);
-                    }
-                    thisAmount += 300 * performance.getAudience();
-                    break;
-                default:
-                    throw new Exception(String.format("알 수 없는 장르: %s", play.getType()));
-            }
+            int thisAmount = amountFor(performance, play);
 
             // 포인트를 적립한다.
             volumeCredits += Math.max(performance.getAudience() - 30, 0);
@@ -47,5 +24,27 @@ public class Statement {
         }
         result.append(String.format("총액: %.2f\n적립 포인트: %d점\n", totalAmount / 100, volumeCredits));
         return result;
+    }
+
+    private int amountFor(Performance performance, Play play) throws Exception {
+        int thisAmount;
+        switch (play.getType()) {
+            case "tragedy": // 비극
+                thisAmount = 40000;
+                if (performance.getAudience() > 30) {
+                    thisAmount += 1000 * (performance.getAudience() - 30);
+                }
+                break;
+            case "comedy": // 희극
+                thisAmount = 30000;
+                if (performance.getAudience() > 20) {
+                    thisAmount += 10000 + 500 * (performance.getAudience() - 20);
+                }
+                thisAmount += 300 * performance.getAudience();
+                break;
+            default:
+                throw new Exception(String.format("알 수 없는 장르: %s", play.getType()));
+        }
+        return thisAmount;
     }
 }
