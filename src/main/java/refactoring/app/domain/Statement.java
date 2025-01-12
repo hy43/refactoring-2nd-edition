691b45e1e5997e5f7971f48c6aee3aad641dea19
hy43 <hyde0401@gmail.com>
2025-01-12 22:32:55 +0900

    refactoring : remove temp variable(play)

--- src/main/java/refactoring/app/domain/Statement.java
+++ src/main/java/refactoring/app/domain/Statement.java
@@ -8,28 +8,31 @@ public class Statement {
         StringBuilder result = new StringBuilder(String.format("청구 내역 (고객명: %s)\n", invoice.getCustomer()));
 
         for (Performance performance : invoice.getPerformances()) {
-            Play play = plays.findByPlayID(performance.getPlayID());
-            int thisAmount = amountFor(performance, play);
+            int thisAmount = amountFor(performance, plays);
 
             // 포인트를 적립한다.
             volumeCredits += Math.max(performance.getAudience() - 30, 0);
 
             // 희극 관객 5명마다 추가 포인트를 제공한다.
-            if ("comedy".equals(play.getType())) {
+            if ("comedy".equals(playFor(performance, plays).getType())) {
                 volumeCredits += performance.getAudience() / 5;
             }
 
             // 청구 내역을 출력한다.
-            result.append(String.format(" %s: %.2f (%d석)\n", play.getName(), (double) thisAmount / 100, performance.getAudience()));
+            result.append(String.format(" %s: %.2f (%d석)\n", playFor(performance, plays).getName(), (double) thisAmount / 100, performance.getAudience()));
             totalAmount += thisAmount;
         }
         result.append(String.format("총액: %.2f\n적립 포인트: %d점\n", totalAmount / 100, volumeCredits));
         return result.toString();
     }
 
-    private int amountFor(Performance performance, Play play) throws Exception {
+    private static Play playFor(Performance performance, Plays plays) {
+        return plays.findByPlayID(performance.getPlayID());
+    }
+
+    private int amountFor(Performance performance, Plays plays) throws Exception {
         int result = 0;
-        switch (play.getType()) {
+        switch (playFor(performance, plays).getType()) {
             case "tragedy": // 비극
                 result = 40000;
                 if (performance.getAudience() > 30) {
@@ -44,7 +47,7 @@ public class Statement {
                 result += 300 * performance.getAudience();
                 break;
             default:
-                throw new Exception(String.format("알 수 없는 장르: %s", play.getType()));
+                throw new Exception(String.format("알 수 없는 장르: %s", playFor(performance, plays).getType()));
         }
         return result;
     }
